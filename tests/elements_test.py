fedfbf41f5362de8b0c076098fa9912d3a5b4745
Zhanna <gorbacheva.zh.v@gmail.com>
2022-07-03 18:59:48 +0300

    feat: modified test

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -9,11 +9,11 @@ class TestElements:
         def test_text_box(self, browser):
             text_box_page = TextBoxPage(browser, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_all_fields()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             time.sleep(4)
             output_name, output_email, output_current_address, output_permanent_address =\
                 text_box_page.check_filled_form()
-            print(output_name)
-            print(output_email)
-            print(output_current_address)
-            print(output_permanent_address)
+            assert full_name == output_name, "The full name does not match"
+            assert email == output_email, "The email does not match"
+            assert current_address == output_current_address, "The current address does not match"
+            assert permanent_address == output_permanent_address, "The permanent address does not match"
