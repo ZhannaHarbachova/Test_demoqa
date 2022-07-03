fbbf533e8f3911b7b8d7f1da7c13f14108d63e2d
Zhanna <gorbacheva.zh.v@gmail.com>
2022-07-03 14:23:44 +0300

    feat: added base functions

---
+++ pages/base_page.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__ (self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.visibility_of_element_located(locator))
+
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.presence_of_element_located(locator))
+
+    def elements_are_present(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        self.browser.execute_script("argument[0].scrollIntoView();", element)
